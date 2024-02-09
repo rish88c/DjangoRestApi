d247fae123069dcd82bcc988b61895ca387046e6
rish88c <rishg98@gmail.com>
2024-02-10 00:59:14 +0530

    Rest Api in Progress

---
+++ gs2/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+# Create your views here.
+def student_create(request):
+    if request.method == 'POST':
+        json_data = request.body
+        print(json_data)
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        serializer = StudentSerializer(data = python_data)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg':'Data Inserted'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data,content_type = 'application/json')
